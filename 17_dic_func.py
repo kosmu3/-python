dd24bff28a16a63d402072a2eb52ed325cc6315f
ohyejin <ohyejin@email.com>
2025-09-12 12:31:03 +0900

    dic 함수소개-문제

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -19,3 +19,23 @@ print(dic.values())
 # list 로 변경해서 values 라는 변수에 담아보자
 values = list(dic.values())
 print(values)
+
+# dic.item() : 사전의 키:값 을 한 쌍으로 가져와 dict_items 로 반환한다.
+# 각 키와 값은() 모양으로 보아 tuple 이다.
+print(dic.items())
+# list 로 변환해 보면 list 안에 각 키와 값이 튜블로 저장되어 있음을 알 수 있다.
+li = list(dic.items())
+print(li)
+
+# 값을 가져오기
+print(dic.get('name'))
+print(dic['phone'])
+
+# dic 안에 있는 모든 키와 값을 키:값 형태로 출력해보자
+# 1. 키를 뽑아낸다음, 키를 가지고 값을 뽑아내는 방법
+for key in dic.keys():
+    print(f'{key}: {dic[key]}')
+
+# 2. 키와 값을 동시에 뽑아낸 다음 거기서 키와 값을 각각 추출하는 방식
+for item in dic.items():
+    print(f'{item[0]}={item[1]}')
