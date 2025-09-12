b79c2b2d50c3b8d445610e071ae092c736e1ab11
ohyejin <ohyejin@email.com>
2025-09-12 12:58:14 +0900

    dic 함수소개-마지막

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -39,3 +39,29 @@ for key in dic.keys():
 # 2. 키와 값을 동시에 뽑아낸 다음 거기서 키와 값을 각각 추출하는 방식
 for item in dic.items():
     print(f'{item[0]}={item[1]}')
+
+members = {
+    'kim':63, 'lee':88, 'park':97, "gang":77, "hwang":100, "ga":65,
+    "na":92, "la":90, "wang":100, "gu":79
+}
+
+# 90이상인 사람의 이름만 출력
+for item in members.items():
+    if item[1]>=90:
+        print(f'이름 : {item[0]}')
+
+# key in dic : 해당 키가 사전에 존재하는지 확인
+# 검색 시작여부를 결정할수 있는 방법
+yn = 'kim'in members
+print(f'kim이 있는가? {yn}')
+
+yn = 'jung'in members
+print(f'jung이 있는가? {yn}')
+
+# update : 이미 있는 키면 수정을, 없는 키면 추가를 하는 향수
+dic.update({'name':'홍길동','age':30,'married':False})
+print(dic)
+
+# dic.clear() : 사전 안의 내용을 모두 지운다.
+dic.clear()
+print(dic)
