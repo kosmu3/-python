6580569ffeafa801c4c1ac6b085c9c2be32a9e8b
ohyejin <ohyejin@email.com>
2025-09-16 16:50:01 +0900

    다중 예외 처리 방식

---
+++ 13_test.py
@@ -0,0 +1,21 @@
+import traceback
+
+text = input('값을 넣으면 숫자로 변환 됩니다.')
+
+
+# try:
+#     num = int(text)
+#     print(f'당신이 입력한 값 : {num} 이 숫자로 변환 되었습니다.')
+# except ValueError:
+#     print('숫자를 입력하세요')
+# finally:
+#     print('끝입니다')
+
+while True:
+    try:
+        text = input('값을 넣으면 숫자로 변환 됩니다.')
+        num = int(text)
+        print(f'당신이 입력한 값 :{num} 이 숫자로 변환 되었습니다.')
+        break
+    except ValueError:
+        print(f'{text} 는 숫자가 아닙니다.')
