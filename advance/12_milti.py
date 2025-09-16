8670f784127c70b52d05218f7be106af8a46fdf4
ohyejin <ohyejin@email.com>
2025-09-16 15:52:44 +0900

    다중 예외 처리 방식

--- advance/12_milti.py
+++ advance/12_milti.py
@@ -1,4 +1,5 @@
 # try 코드 안에서 여러개의 예외가 발생한 경우...
+import traceback
 
 # 1. 생겨난 예외마다 각기 다른 처리를 해주고 싶은 때
 try:
@@ -13,5 +14,5 @@ except ValueError:
 # 2. 어떤 예외가 발생하던지 동일한 처리를 하고 싶을때
 try:
     pass
-except Exception:
-    pass
+except Exception as e: # 예외의 최상위 부모이기 때문에 다른 자식 예외들이 모두 들어올 수 있다.
+    traceback.print_exc()
